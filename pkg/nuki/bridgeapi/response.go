e35825eb52715c9e3bd814c58218c37940506e76
Christian Schmitz <88237+christianschmizz@users.noreply.github.com>
2020-11-21 10:20:59 +0100

    Use appropriate naming - apiResponse gets APIResponseHandler

--- pkg/nuki/bridgeapi/response.go
+++ pkg/nuki/bridgeapi/response.go
@@ -8,30 +8,32 @@ import (
 	"net/http"
 )
 
-type apiResponse struct {
-	path         string
-	url          string
+// APIResponseHandler eases working with a http response
+type APIResponseHandler struct {
 	httpResponse *http.Response
 }
 
-func NewApiResponse(resp *http.Response) (*apiResponse, error) {
+// NewAPIResponseHandler returns a new response handler
+func NewAPIResponseHandler(resp *http.Response) (*APIResponseHandler, error) {
 	readBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read response body for '%s': %w", resp.Request.URL, err)
 	}
 	resp.Body = ioutil.NopCloser(bytes.NewReader(readBody))
-	return &apiResponse{
+	return &APIResponseHandler{
 		httpResponse: resp,
 	}, nil
 }
 
-func (r *apiResponse) Decode(v interface{}) error {
+// Decode decodes the response's body to the given value
+func (r *APIResponseHandler) Decode(v interface{}) error {
 	if err := json.NewDecoder(r.httpResponse.Body).Decode(v); err != nil {
 		return fmt.Errorf("decoding failed: %w\n\n%+v", err, r.httpResponse.Body)
 	}
 	return nil
 }
 
-func (r *apiResponse) Is(statusCode int) bool {
+// Is checks the response for the given status code
+func (r *APIResponseHandler) Is(statusCode int) bool {
 	return r.httpResponse.StatusCode == statusCode
 }
