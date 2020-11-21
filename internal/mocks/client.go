8c2246b710836b5ddec4fb2402fad3b753f3a996
Christian Schmitz <88237+christianschmizz@users.noreply.github.com>
2020-11-21 10:16:30 +0100

    Fix linter annotations for the FileResponseClient

--- internal/mocks/client.go
+++ internal/mocks/client.go
@@ -7,20 +7,27 @@ import (
 	"os"
 )
 
-type fileResponseClient struct {
+// The FileResponseClient fakes the execution of a request and returns always
+// the contents of the given file with the given HTTP status.
+type FileResponseClient struct {
 	name   string
 	status int
 }
 
-func NewFileResponseClient(name string, status int) *fileResponseClient {
-	return &fileResponseClient{
+// NewFileResponseClient instantiates a new client
+func NewFileResponseClient(name string, status int) *FileResponseClient {
+	return &FileResponseClient{
 		name:   name,
 		status: status,
 	}
 }
 
-func (c *fileResponseClient) Do(*http.Request) (*http.Response, error) {
-	f, _ := os.Open(c.name)
+// Do fakes the given request and returns the defined file content and status code
+func (c *FileResponseClient) Do(*http.Request) (*http.Response, error) {
+	f, err := os.Open(c.name)
+	if err != nil {
+		return nil, err
+	}
 	r := ioutil.NopCloser(bufio.NewReader(f))
 	return &http.Response{
 		StatusCode: c.status,
